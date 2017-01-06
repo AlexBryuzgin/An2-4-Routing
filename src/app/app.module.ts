7167c2c0973bfdc1d90d7a3e6c3086c39f554a7f
Aliaksandr Bruzgin <Aliaksandr_Bruzgin@epam.com>
2017-01-06 13:44:42 +0300

    tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NgModuleFactoryLoader } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module'
+
+import { routing } from './app.routing';
+
+import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service';
+import { DialogService } from './services/dialog.service'
+
 import { TodoAppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    TodoAppComponent
+    TodoAppComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    TasksModule,
+    UsersModule,
+    AdminModule,
+    routing
   ],
   providers: [
+    AuthGuard,
+    AuthService, 
+    DialogService
   ],
   entryComponents: [TodoAppComponent],
   bootstrap: [TodoAppComponent]
